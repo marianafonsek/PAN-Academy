17c8ec62d9fc9f54117a3480d773ade2cb092559
Mariana Fonseca <91077669+marianafonsek@users.noreply.github.com>
2021-10-20 18:26:49 -0300

    metodos estaticos

---
+++ Modulo_Java/Aula6-MetodosEstatico/src/main/java/com/example/demo/MetodosEstaticoApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetodosEstaticoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MetodosEstaticoApplication.class, args);
+	}
+
+}
