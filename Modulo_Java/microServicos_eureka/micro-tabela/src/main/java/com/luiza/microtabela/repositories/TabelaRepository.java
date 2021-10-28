0a8aa245616c853e7e4c3e6f76515afa5c855071
Mariana Fonseca <91077669+marianafonsek@users.noreply.github.com>
2021-10-28 19:00:26 -0300

    Delete Modulo_Java/microServicos_eureka directory

--- Modulo_Java/microServicos_eureka/micro-tabela/src/main/java/com/luiza/microtabela/repositories/TabelaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.luiza.microtabela.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.luiza.microtabela.model.Tabela;
-
-public interface TabelaRepository extends JpaRepository<Tabela, Long> {
-
-}
