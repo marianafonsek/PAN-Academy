ef856cc575d45978a4473fb14ef20c8e33f37719
Mariana Fonseca <91077669+marianafonsek@users.noreply.github.com>
2021-10-29 14:41:33 -0300

    tarefa do dia 28_10 - Json com While

---
+++ Tarefas_Complementares/tarefaExtraJson/src/test/java/com/tarefaExtraJson/TarefaExtraJsonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tarefaExtraJson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TarefaExtraJsonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
