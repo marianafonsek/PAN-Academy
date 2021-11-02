79d4b3cdee82fbe546e63a93ae9dab7f2cb48222
Mariana Fonseca <91077669+marianafonsek@users.noreply.github.com>
2021-11-01 22:42:05 -0300

    Gson

---
+++ Modulo_Java/Aula19-Gson/src/main/java/com/gson/GsonApplication.java
@@ -0,0 +1,13 @@
+package com.gson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GsonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GsonApplication.class, args);
+	}
+
+}
---
+++ Modulo_Java/Aula19-Gson/src/test/java/com/gson/GsonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GsonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
