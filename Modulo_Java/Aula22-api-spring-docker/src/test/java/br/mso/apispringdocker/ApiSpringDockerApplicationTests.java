a905838031019db658d11c1d7065d08e2e1a2016
Mariana Fonseca <91077669+marianafonsek@users.noreply.github.com>
2021-11-10 16:50:35 -0300

    Docker

---
+++ Modulo_Java/Aula22-api-spring-docker/src/test/java/br/mso/apispringdocker/ApiSpringDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.mso.apispringdocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiSpringDockerApplicationTests {
+  
+  @Test
+  void contextLoads() {
+  }
+  
+}
